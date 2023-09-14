0b6ef988e8938b813c0692e22628df41c8772899
AbdulRahman Maniar <ar1vu@outlook.com>
2023-09-15 00:01:53 +0400

    Remove extra files

--- extra/script/sheet-url.js
+++
@@ -1,1 +0,0 @@
-const SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQbAdmFDP7AtQ7G-d_ULLC8WrRQUaoSeSAiwZgXuqN9Zo4QSvm7OEMBbr_6qXIB-p1OpiyZzW33BP77/pub?gid=973777172&single=true&output=csv';
